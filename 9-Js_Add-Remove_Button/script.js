9ee9d2b134d662eb6d7c570d13b169475c876e9d
Ferhat Yigit <yiitferhat@gmail.com>
2024-12-25 13:53:06 +0300

    commit

--- 9-Js_Add-Remove_Button/script.js
+++ 9-Js_Add-Remove_Button/script.js
@@ -1,8 +1,37 @@
 
-
+document.querySelector("body").classList.add("text-center")
+document.querySelector("h1").classList.add("text-danger")
+document.querySelector(".item-list h2").classList.add("text-bg-warning")
 //! Selectors
 
 const input = document.querySelector("#input")
-const butonumuz = document.getElementById("btn")
+const EkleBtn = document.getElementById("btn")
+const SilBtn = document.getElementById("btn2")
 const ul = document.querySelector("ul")
+
+// değişken Tanımlamaları yaptık. 
+//* Event tanımlamalarını yapalım.
+
+EkleBtn.addEventListener("click", () => {
+    input.focus()
+    if (!input.value) {
+        alert("Boş iş yapamazsınız.")
+        return
+    }
+    const li = document.createElement("li")
+    li.textContent = input.value
+    ul.appendChild(li)
+    input.value = ""
+})
+
+SilBtn.addEventListener("click", () => {
+    input.focus()
+    const SilinecekSonEleman = ul.lastElementChild;
+    if (SilinecekSonEleman){
+        ul.removeChild(SilinecekSonEleman)
+    } else {
+        alert("Silinecek görev kalmadı.")
+    }
+
+})
 
